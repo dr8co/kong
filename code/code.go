721c486c5ea6e32c50809d8d6074e9012c14a34c
Ian Duncan <76043277+dr8co@users.noreply.github.com>
2025-09-26 19:31:58 +0300

    Implement a simple bytecode disassembler

--- code/code.go
+++ code/code.go
@@ -3,6 +3,7 @@ package code
 import (
 	"encoding/binary"
 	"fmt"
+	"strings"
 )
 
 type Instructions []byte
@@ -51,4 +52,54 @@ func Make(op Opcode, operands ...int) []byte {
 		offset += width
 	}
 	return instruction
+}
+
+func (ins Instructions) String() string {
+	var out strings.Builder
+
+	i := 0
+	for i < len(ins) {
+		def, err := Lookup(ins[i])
+		if err != nil {
+			_, _ = fmt.Fprintf(&out, "ERROR: %s\n", err)
+			continue
+		}
+		operands, read := ReadOperands(def, ins[i+1:])
+		_, _ = fmt.Fprintf(&out, "%04d %s\n", i, ins.fmtInstruction(def, operands))
+		i += read + 1
+	}
+
+	return out.String()
+}
+
+func (ins Instructions) fmtInstruction(def *Definition, operands []int) string {
+	operandCount := len(def.OperandWidths)
+
+	if len(operands) != operandCount {
+		return fmt.Sprintf("ERROR: operand len %d does not match defined %d\n", len(operands), operandCount)
+	}
+
+	switch operandCount {
+	case 1:
+		return fmt.Sprintf("%s %d", def.Name, operands[0])
+	}
+	return fmt.Sprintf("ERROR: unhandled operandCount for %s\n", def.Name)
+}
+
+func ReadOperands(def *Definition, ins Instructions) ([]int, int) {
+	operands := make([]int, len(def.OperandWidths))
+	offset := 0
+
+	for i, width := range def.OperandWidths {
+		switch width {
+		case 2:
+			operands[i] = int(ReadUint16(ins[offset:]))
+		}
+		offset += width
+	}
+	return operands, offset
+}
+
+func ReadUint16(ins Instructions) uint16 {
+	return binary.BigEndian.Uint16(ins)
 }
