5f883ef98b56b9a6ac13f2306bb970a2fe63fd58
Ian Duncan <76043277+dr8co@users.noreply.github.com>
2025-09-30 12:55:30 +0300

    Implement a stack frame

---
+++ vm/frame.go
@@ -0,0 +1,19 @@
+package vm
+
+import (
+	"github.com/dr8co/kong/code"
+	"github.com/dr8co/kong/object"
+)
+
+type Frame struct {
+	fn *object.CompiledFunction
+	ip int
+}
+
+func NewFrame(fn *object.CompiledFunction) *Frame {
+	return &Frame{fn: fn, ip: -1}
+}
+
+func (f *Frame) Instructions() code.Instructions {
+	return f.fn.Instructions
+}
