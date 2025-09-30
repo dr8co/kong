dcb249c6c00f1f0e86929642f57ae26dd8ebee35
Ian Duncan <76043277+dr8co@users.noreply.github.com>
2025-09-30 13:12:52 +0300

    Implement `OpReturn` instruction

--- vm/vm.go
+++ vm/vm.go
@@ -193,6 +193,15 @@ func (vm *VM) Run() error {
 				return err
 			}
 
+		case code.OpReturn:
+			vm.popFrame()
+			vm.pop()
+
+			err := vm.push(Null)
+			if err != nil {
+				return err
+			}
+
 		case code.OpReturnValue:
 			returnValue := vm.pop()
 			vm.popFrame()
