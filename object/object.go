dc2f23a5e1f516da5d9a9e91f5d3676e8356301f
Ian Duncan <76043277+dr8co@users.noreply.github.com>
2025-09-30 10:35:17 +0300

    Add `CompiledFunction` object to support compilation of function literals

--- object/object.go
+++ object/object.go
@@ -7,19 +7,21 @@ import (
 	"strings"
 
 	"github.com/dr8co/kong/ast"
+	"github.com/dr8co/kong/code"
 )
 
 const (
-	INTEGER_OBJ      = "INTEGER"
-	BOOLEAN_OBJ      = "BOOLEAN"
-	STRING_OBJ       = "STRING"
-	NULL_OBJ         = "NULL"
-	RETURN_VALUE_OBJ = "RETURN_VALUE"
-	ERROR_OBJ        = "ERROR"
-	FUNCTION_OBJ     = "FUNCTION"
-	BUILTIN_OBJ      = "BUILTIN"
-	ARRAY_OBJ        = "ARRAY"
-	HASH_OBJ         = "HASH"
+	INTEGER_OBJ           = "INTEGER"
+	BOOLEAN_OBJ           = "BOOLEAN"
+	STRING_OBJ            = "STRING"
+	NULL_OBJ              = "NULL"
+	RETURN_VALUE_OBJ      = "RETURN_VALUE"
+	ERROR_OBJ             = "ERROR"
+	FUNCTION_OBJ          = "FUNCTION"
+	BUILTIN_OBJ           = "BUILTIN"
+	ARRAY_OBJ             = "ARRAY"
+	HASH_OBJ              = "HASH"
+	COMPILED_FUNCTION_OBJ = "COMPILED_FUNCTION_OBJ"
 )
 
 type ObjectType string
@@ -181,3 +183,10 @@ func (h *Hash) Inspect() string {
 type Hashable interface {
 	HashKey() HashKey
 }
+
+type CompiledFunction struct {
+	Instructions code.Instructions
+}
+
+func (c *CompiledFunction) Type() ObjectType { return COMPILED_FUNCTION_OBJ }
+func (c *CompiledFunction) Inspect() string  { return fmt.Sprintf("CompiledFunction[%p]", c) }
