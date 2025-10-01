0dead939bd936eb011b7b1cc751086290cde3193
Ian Duncan <76043277+dr8co@users.noreply.github.com>
2025-10-01 13:58:34 +0300

    Add support for free variables in the symbol table

--- compiler/symbol_table.go
+++ compiler/symbol_table.go
@@ -6,6 +6,7 @@ const (
 	GlobalScope  SymbolScope = "GLOBAL"
 	LocalScope   SymbolScope = "LOCAL"
 	BuiltinScope SymbolScope = "BUILTIN"
+	FreeScope    SymbolScope = "FREE"
 )
 
 type Symbol struct {
@@ -18,11 +19,13 @@ type SymbolTable struct {
 	Outer          *SymbolTable
 	store          map[string]Symbol
 	numDefinitions int
+	FreeSymbols    []Symbol
 }
 
 func NewSymbolTable() *SymbolTable {
 	return &SymbolTable{
-		store: make(map[string]Symbol),
+		store:       make(map[string]Symbol),
+		FreeSymbols: []Symbol{},
 	}
 }
 
@@ -49,6 +52,12 @@ func (s *SymbolTable) Resolve(name string) (Symbol, bool) {
 	obj, ok := s.store[name]
 	if !ok && s.Outer != nil {
 		obj, ok = s.Outer.Resolve(name)
+		if ok {
+			if obj.Scope != GlobalScope && obj.Scope != BuiltinScope {
+				free := s.defineFree(obj)
+				return free, true
+			}
+		}
 	}
 	return obj, ok
 }
@@ -56,5 +65,15 @@ func (s *SymbolTable) Resolve(name string) (Symbol, bool) {
 func (s *SymbolTable) DefineBuiltin(index int, name string) Symbol {
 	symbol := Symbol{Name: name, Index: index, Scope: BuiltinScope}
 	s.store[name] = symbol
+	return symbol
+}
+
+func (s *SymbolTable) defineFree(original Symbol) Symbol {
+	s.FreeSymbols = append(s.FreeSymbols, original)
+	symbol := Symbol{Name: original.Name, Index: len(s.FreeSymbols) - 1}
+
+	symbol.Scope = FreeScope
+	s.store[original.Name] = symbol
+
 	return symbol
 }
